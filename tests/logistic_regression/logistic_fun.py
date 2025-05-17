8c298b7e8fd80727f28c88de084695b4d4073dbc
Limao Chang <80520563+LimaoC@users.noreply.github.com>
2025-05-17 16:16:52 +1000

    test: vectorise logistic loss functions

--- tests/logistic_regression/logistic_fun.py
+++ tests/logistic_regression/logistic_fun.py
@@ -6,25 +6,25 @@ from scipy.special import expit
 
 def logistic_f(A, b, reg_param=0):
     """Logistic loss function with l2 regularisation"""
-    n, d = A.shape
 
     def phi(t):
         """Computes phi(t) = ln(1 + e^t) using the log-sum-exp trick"""
-        c = max(0, t)
+        c = np.maximum(0, t)
         return c + np.log(np.exp(-c) + np.exp(t - c))
 
-    return lambda x: sum(
-        phi(A[i].dot(x)) - b[i].item() * A[i].dot(x) for i in range(n)
-    ) + reg_param / 2 * (np.linalg.norm(x) ** 2)
+    def loss(x):
+        Ax = A @ x
+        reg_term = (reg_param / 2) * (np.linalg.norm(x) ** 2)
+        return np.sum(phi(Ax) - b.flatten() * Ax) + reg_term
+
+    return loss
 
 
 def logistic_grad_f(A, b, reg_param=0) -> Callable[[np.ndarray], np.ndarray]:
     """Logistic loss gradient function with l2 regularisation"""
-    n, d = A.shape
-
     grad_phi = expit
 
-    return (
-        lambda x: sum(A[i] * grad_phi(A[i].dot(x)) - b[i] * A[i] for i in range(n))
-        + reg_param * x
-    )
+    def loss(x):
+        return A.T @ (grad_phi(A @ x) - b.flatten()) + reg_param * x
+
+    return loss
